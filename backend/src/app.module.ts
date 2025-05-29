c3e50eb526bae4db35c735fd946e843cee279c85
cedricnator <c.kirmayr01@ufromail.cl>
2025-05-28 21:14:52 -0400

    feat[back]: implement Auth, Files, and Signature modules with controllers, services, and DTOs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import { SignatureModule } from './signature/signature.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FilesModule, AuthModule, SignatureModule],
   controllers: [AppController],
   providers: [AppService],
 })
