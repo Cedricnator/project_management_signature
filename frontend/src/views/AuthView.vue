dc31b1af2232a595ba361c19a455429fd50dd05d
alex2022-lab <111999591+alex2022-lab@users.noreply.github.com>
2025-06-06 08:54:28 -0400

    Setup AuthView

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -3,7 +3,21 @@
 <template>
     <main>
         <div class="flex bg-blue-500">
-            <p class="flex">AUTH</p>
+            <p class="flex">Firma Digital</p>
+        </div>
+        <div>
+            <h1 class="mt-10 text-center">Iniciar Sesión</h1>
+            <h3 class="text-center">Bienvenido de vuelta</h3>
+            <form>
+                <div class="flex justify-center items-center pr-25">
+                <div class="mt-10">
+                <label for="text">Correo Electrónico</label> <br>
+                <input class="border"> <br>
+                <label for="text">Contraseña</label> <br>
+                <input class="border"> <br>
+                </div>
+                </div>
+            </form>
         </div>
     </main>
 </template>
