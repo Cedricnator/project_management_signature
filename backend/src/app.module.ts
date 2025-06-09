633ad984cf8817575620eccdebffd268fb7c34fe
cedricnator <c.kirmayr01@ufromail.cl>
2025-06-08 22:25:17 -0400

    fix(app): remove duplicate ConfigModule import in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,20 +7,18 @@ import { AuthModule } from './auth/auth.module';
 import { SignatureModule } from './signature/signature.module';
 import { DatabaseModule } from './common/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
 import { CommonModule } from './common/common.module';
 import { SecurityModule } from './security/security.module';
 
 @Module({
   imports: [
     DatabaseModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     FilesModule,
     AuthModule,
     SignatureModule,
     UsersModule,
-    ConfigModule.forRoot({ isGlobal: true }),
     CommonModule,
     SecurityModule,
   ],
