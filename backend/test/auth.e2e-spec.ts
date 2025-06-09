ea858cf2c18ae627e875b6d7d3abd1134a3f2cdb
nico <n.arias10@ufromail.cl>
2025-06-08 21:21:43 -0400

    feat(tests): enhance auth e2e tests with TypeORM and ConfigModule integration

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -1,20 +1,54 @@
 import * as request from 'supertest';
 
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AuthModule } from '../src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../src/users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 describe('Auth E2E Tests', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AuthModule],
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'test',
+          password: 'test',
+          database: 'test',
+          entities: [User],
+          synchronize: true,
+          dropSchema: true,
+          logging: false,
+        }),
+        AuthModule,
+        ConfigModule.forRoot(),
+      ],
     }).compile();
-    app = moduleRef.createNestApplication();
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
+  /*
   it('/Post /auth/login', async () => {
-    return request(app.getHttpServer()).get('/auth/login').expect(404);
+    return request(app.getHttpServer()).post('/auth/login').expect(404);
+  });
+  
+   */
+  it('/Post /auth/register', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/register')
+      .set('Content-Type', 'application/json')
+      .send({
+        email: 'test@test.com',
+        password: 'test1234',
+        firstName: 'Test',
+        lastName: 'User',
+      });
+    expect(response.status).toBe(201);
   });
   afterAll(async () => {
     await app.close();
